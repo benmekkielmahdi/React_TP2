6a977b61d1dffab5dae9c4452767bdf27dc71696
ben mekki el mahdi <95963227+benmekkielmahdi@users.noreply.github.com>
2025-11-09 23:23:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Compteur from './Compteur';
+import MessageBouton from './MessageBouton';
+import FormulaireNom from './FormulaireNom';
+
+import CompteurEffet from './CompteurEffet';
+
+function App() {
+  return (
+    <div>
+      <Compteur />
+      <MessageBouton />
+      <FormulaireNom />
+      <CompteurEffet />
+    </div>
+  );
+}
+
+export default App;
